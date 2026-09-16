9ba43778c0d6983d613df1cfc541d0040cea4956
NishitaParakh <parakhnishita14@gmail.com>
2026-09-16 23:20:55 +0530

    Add Interview Model

--- src/tracker/Interview.java
+++ src/tracker/Interview.java
@@ -1,3 +1,40 @@
+package tracker;
+
 public class Interview {
-    
+
+    private int applId;
+    private String interviewDate;
+    private String interviewTime;
+    private String interviewType;
+
+    public Interview(int applId, String interviewDate,
+                     String interviewTime, String interviewType) {
+        this.applId = applId;
+        this.interviewDate = interviewDate;
+        this.interviewTime = interviewTime;
+        this.interviewType = interviewType;
+    }
+
+    public int getApplId() {
+        return applId;
+    }
+
+    public String getInterviewDate() {
+        return interviewDate;
+    }
+
+    public String getInterviewTime() {
+        return interviewTime;
+    }
+
+    public String getInterviewType() {
+        return interviewType;
+    }
+
+    public void displayInterview() {
+        System.out.println("Application ID : " + applId);
+        System.out.println("Interview Date : " + interviewDate);
+        System.out.println("Interview Time : " + interviewTime);
+        System.out.println("Interview Type : " + interviewType);
+    }
 }
